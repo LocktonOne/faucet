c1321607d369d92edb1a3cf41c53b050c83328cf
mark.cherepovskyi <mark.cherepovskyi@gmail.com>
2022-12-06 19:36:18 +0200

    error with dorman connection

--- internal/config/main.go
+++ internal/config/main.go
@@ -5,6 +5,7 @@ import (
 	"gitlab.com/distributed_lab/kit/copus"
 	"gitlab.com/distributed_lab/kit/copus/types"
 	"gitlab.com/distributed_lab/kit/kv"
+	doormanCfg "gitlab.com/tokene/doorman/connector/config"
 	"gitlab.com/tokene/faucet/internal/signature"
 )
 
@@ -15,6 +16,7 @@ type Config interface {
 	AuthConfiger
 	signature.Signerer
 	EthRPCConfiger
+	doormanCfg.DoormanConfiger
 }
 
 type config struct {
@@ -25,16 +27,19 @@ type config struct {
 	EthRPCConfiger
 	signature.Signerer
 	getter kv.Getter
+
+	doormanCfg.DoormanConfiger
 }
 
 func New(getter kv.Getter) Config {
 	return &config{
-		getter:         getter,
-		Signerer:       signature.NewSignerer(getter),
-		EthRPCConfiger: NewEthRPCConfiger(getter),
-		AuthConfiger:   NewAuthConfiger(getter),
-		Copuser:        copus.NewCopuser(getter),
-		Listenerer:     comfig.NewListenerer(getter),
-		Logger:         comfig.NewLogger(getter, comfig.LoggerOpts{}),
+		getter:          getter,
+		DoormanConfiger: doormanCfg.NewDoormanConfiger(getter),
+		Signerer:        signature.NewSignerer(getter),
+		EthRPCConfiger:  NewEthRPCConfiger(getter),
+		AuthConfiger:    NewAuthConfiger(getter),
+		Copuser:         copus.NewCopuser(getter),
+		Listenerer:      comfig.NewListenerer(getter),
+		Logger:          comfig.NewLogger(getter, comfig.LoggerOpts{}),
 	}
 }
--- internal/service/middlewares/login.go
+++ internal/service/middlewares/login.go
@@ -1,30 +1,16 @@
 package middlewares
 
 import (
-	"gitlab.com/distributed_lab/ape"
-	"gitlab.com/distributed_lab/ape/problems"
-	"gitlab.com/tokene/faucet/internal/service/helpers"
 	"net/http"
 )
 
 func Login() func(http.Handler) http.Handler {
 	return func(next http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			client := &http.Client{}
-			req, _ := http.NewRequest("GET", helpers.AuthConfig(r).Endpoint, nil)
-			req.Header.Set("Authorization", r.Header.Get("Authorization"))
-			authResponse, err := client.Do(req)
-			defer authResponse.Body.Close()
-			if err != nil {
-				helpers.Log(r).WithError(err).Error("failed to get response from nonce-auth-svc")
-				ape.Render(w, problems.InternalError())
-				return
-			}
-			if authResponse.StatusCode != 200 {
-				helpers.Log(r).WithError(err).Error("unauthorized")
-				ape.Render(w, problems.Unauthorized())
-				return
-			}
+
+			//todo parse JWT
+			//if address == address from request
+			//use doorman.CheckPermission
 			next.ServeHTTP(w, r)
 		})
 	}
